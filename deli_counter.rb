2730aa6c9ceff0ca525456e86a78fad8da003b75
David Moore <hey@iamdavidmoore.com>
2018-04-19 02:44:14 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line
     puts "The line is currently empty."
   elsif katz_deli != []
     katz_deli.each_with_index{|person, place_number|
-        line_status_string += "#{place_number+1} #{person}"
+        line_status_string += "#{place_number+1}. #{person}"
     }
     puts line_status_string
   end
