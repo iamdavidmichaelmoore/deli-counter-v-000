e708046e83bbc8b6cee99065ee2fd44a713ab2d8
David Moore <hey@iamdavidmoore.com>
2018-04-19 02:20:44 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+katz_deli = []
+
+def line
+  if katz_deli == nil
+    puts "The line is currently empty."
+  elsif katz_deli != nil
+    katz_deli.each do |line_status|
+      puts line_status
+    end
+  end
+end
+    
+    
