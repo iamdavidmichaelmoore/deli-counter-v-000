a035c3c42070086f5aaada6ecc8f982dbaba5bca
David Moore <hey@iamdavidmoore.com>
2018-04-19 03:16:54 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,12 +4,12 @@ katz_deli = []
 def line(array)
   line_status_string = "The line is currently:"
   if array == []
-    "The line is currently empty."
+    puts "The line is currently empty."
   elsif array != []
     array.each_with_index {|person, place_number|
       line_status_string += " #{place_number+1}. #{person}"
     }
-    line_status_string
+    puts line_status_string
   end
 end
 
@@ -18,9 +18,9 @@ def take_a_number(array, name)
   call = ""
   if array == []
     array.push(name)
-    "Welcome, #{array[0]}. You are number #{array.length} in line."
+    puts "Welcome, #{array[0]}. You are number #{array.length} in line."
   elsif array != []
     array.push(name)
-    "Welcome, #{array.last}. You are number #{array.length} in line."
+    puts "Welcome, #{array.last}. You are number #{array.length} in line."
   end
 end
