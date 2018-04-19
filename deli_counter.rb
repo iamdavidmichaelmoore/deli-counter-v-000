259f91d306049a40637ba6525046807b1e32548d
David Moore <hey@iamdavidmoore.com>
2018-04-19 02:38:40 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,17 @@
 katz_deli = []
 
 def line
+  line_status_string = ""
   if katz_deli == nil
     puts "The line is currently empty."
   elsif katz_deli != nil
-    katz_deli.each do |line_status|
-      puts line_status
-    end
+    katz_deli.each_with_index{|person, place_number|
+        line_status_string =
+    }
+    puts line_status_string
   end
 end
-    
-    
+
+def take_a_number(line, name)
+  update_line = line.push(name)
+  puts
