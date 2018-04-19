53a0503301064f69b5a970f42d31067b000d1fc8
David Moore <hey@iamdavidmoore.com>
2018-04-19 02:46:42 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,12 @@
 katz_deli = []
 
 def line(array)
-  line_status_string = "The line is currently: "
+  line_status_string = "The line is currently:"
   if array == []
     puts "The line is currently empty."
   elsif array != []
     array.each_with_index{|person, place_number|
-        line_status_string += "#{place_number+1}. #{person}"
+        line_status_string += " #{place_number+1}. #{person}"
     }
     puts line_status_string
   end
