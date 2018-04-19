0342623882b33bb7538021ee79137640ace2d82b
David Moore <hey@iamdavidmoore.com>
2018-04-19 02:59:49 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,17 +1,26 @@
 katz_deli = []
 
+#gives status of line
 def line(array)
   line_status_string = "The line is currently:"
   if array == []
     puts "The line is currently empty."
   elsif array != []
-    array.each_with_index{|person, place_number|
-        line_status_string += " #{place_number+1}. #{person}"
+    array.each_with_index {|person, place_number|
+      line_status_string += " #{place_number+1}. #{person}"
     }
     puts line_status_string
   end
 end
 
-#def take_a_number(line, name)
-#  update_line = line.push(name)
-#  puts
+#takes status of line and person's name, then announces their place in line
+def take_a_number(array, name)
+  if array == []
+    array.push(name)
+    #index_to_place_number = array.length + 1
+    #puts "Welcome, #{name}. You are number #{}"
+    array.each_with_index {|person, place_number|
+      puts "Welcome, #{person}. You are number #{place_number+1} in line."
+    }
+  update_line = line.push(name)
+  puts
